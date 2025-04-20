f71a9e9c6e47dac89a1bade474d7a1fe87bf0c91
Francesco <Francesco@DESKTOP-9B10G4G>
2025-04-20 19:44:45 +0200

    a

--- Slot-machine/src/controller/Controller.java
+++ Slot-machine/src/controller/Controller.java
@@ -2,6 +2,9 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.SwingUtilities;
+
 import model.SlotMachine;
 import view.GameWindow;
 
@@ -20,24 +23,43 @@ public class Controller implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
 	window.getBtnGioca().setEnabled(false);
 	slot.gioca();
-	slot.generaNumeri();
-	int[] n = slot.getNumeri();
-	window.getBox1().setText(Integer.toString(n[0]));
-	window.getBox2().setText(Integer.toString(n[1]));
-	window.getBox3().setText(Integer.toString(n[2]));
-	int nu = slot.getNumeriUguali();
-	if (nu == 0) {
-	    window.getLblVincita().setText("hai perso");
-	} else if (nu == 2) {
-	    window.getLblVincita().setText("hai vinto 3 monete");
-	} else {
-	    window.getLblVincita().setText("<html><center>JACKPOT</center><br>hai vinto " + slot.getJackpot() + " monete</html>");
-	}
-	window.getLblMonete().setText("<html><center>MONETE<br>" + Integer.toString(slot.getMonete()) + "</center></html>");
-	window.getBtnGioca().setEnabled(true);
+	new Thread(()-> {
+	    for(int i=0;i<10;i++) {
+		for(int j=0;j<10;j++) {
+		    int c = j;
+		    try {
+			Thread.sleep(10);
+		    }catch(InterruptedException ex) {
+			ex.printStackTrace();
+		    }
+		    SwingUtilities.invokeLater(()->{
+			window.getBox1().setText(Integer.toString(c));
+			window.getBox2().setText(Integer.toString(c));
+			window.getBox3().setText(Integer.toString(c));
+		    });
+		}
+	    }	
+	    	slot.generaNumeri();
+		int[] n = slot.getNumeri();
+		SwingUtilities.invokeLater(()->{
+		    	window.getBox1().setText(Integer.toString(n[0]));
+			window.getBox2().setText(Integer.toString(n[1]));
+			window.getBox3().setText(Integer.toString(n[2]));
+		    });
+		int nu = slot.getNumeriUguali();
+		if (nu == 0) {
+		    window.getLblVincita().setText("hai perso");
+		} else if (nu == 2) {
+		    window.getLblVincita().setText("hai vinto 3 monete");
+		} else {
+		    window.getLblVincita().setText("<html><center>JACKPOT</center><br>hai vinto " + slot.getJackpot() + " monete</html>");
+		}
+		window.getLblMonete().setText("<html><center>MONETE<br>" + Integer.toString(slot.getMonete()) + "</center></html>");
+		window.getBtnGioca().setEnabled(true);
+	}).start();
+	
     }
 
 }
