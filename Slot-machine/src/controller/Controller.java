267cafcfc29ea35b62e6149b62511b064f9a1deb
Francesco <Francescoguardiano100@gmail.com>
2025-04-22 17:36:40 +0200

    finished (i think)

--- Slot-machine/src/controller/Controller.java
+++ Slot-machine/src/controller/Controller.java
@@ -11,73 +11,79 @@ import model.SlotMachine;
 import view.GameWindow;
 
 public class Controller implements ActionListener {
-    private SlotMachine slot;
-    private GameWindow window;
-
-    public Controller() {
-	window = new GameWindow();
-	slot = new SlotMachine();
-	window.getLblMonete()
-		.setText("<html><center>MONETE<br>" + Integer.toString(slot.getMonete()) + "</center></html>");
-	window.setVisible(true);
-	window.getBtnGioca().addActionListener(this);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-	window.getBtnGioca().setEnabled(false);
-	slot.gioca();
-	window.getLblVincita().setText("<html>Giocata in corso</html>");
-	new Thread(() -> {
-	    for (int i = 0; i < 11; i++) {
-		int cc = i;
-		String txt = "GIOCANDO";
-		SwingUtilities.invokeLater(() -> {
-		    if (cc == 0 || cc == 4 || cc == 8) {
-			window.getLblVincita().setText(txt);
-		    } else if (cc == 1 || cc == 5 || cc == 9) {
-			window.getLblVincita().setText(txt+".");
-		    } else if (cc == 2 || cc == 6 || cc == 10) {
-			window.getLblVincita().setText(txt+"..");
-		    } else if (cc == 3 || cc == 7 || cc==11) {
-			window.getLblVincita().setText(txt+"...");
-		    }
-		});
-		for (int j = 0; j < 10; j++) {
-		    int c = j;
-		    try {
-			Thread.sleep(10);
-		    } catch (InterruptedException ex) {
-			ex.printStackTrace();
-		    }
-		    SwingUtilities.invokeLater(() -> {
-			window.getBox1().setText(Integer.toString(c));
-			window.getBox2().setText(Integer.toString(c));
-			window.getBox3().setText(Integer.toString(c));
-		    });
-		}
-	    }
-	    slot.generaNumeri();
-	    int[] n = slot.getNumeri();
-	    SwingUtilities.invokeLater(() -> {
-		window.getBox1().setText(Integer.toString(n[0]));
-		window.getBox2().setText(Integer.toString(n[1]));
-		window.getBox3().setText(Integer.toString(n[2]));
-	    });
-	    int nu = slot.getNumeriUguali();
-	    if (nu == 0) {
-		window.getLblVincita().setText("Hai perso");
-	    } else if (nu == 2) {
-		window.getLblVincita().setText("<html><center>HAI<br> VINTO 3 MONETE!</center></html>");
-	    } else {
-		window.getLblVincita().setText(
-			"<html><center>JACKPOT<br>HAI VINTO " + slot.getJackpot() + " MONETE!</center></html>");
-	    }
-	    window.getLblMonete()
-		    .setText("<html><center>MONETE<br>" + Integer.toString(slot.getMonete()) + "</center></html>");
-	    window.getBtnGioca().setEnabled(true);
-	}).start();
-
-    }
-
+	private SlotMachine slot;
+	private GameWindow window;
+	
+	public Controller() {
+		window = new GameWindow();
+		slot = new SlotMachine();
+		window.getLblMonete().setText("<html><center>MONETE<br>" + Integer.toString(slot.getMonete())
+				+ "</center></html>");
+		window.setVisible(true);
+		window.getBtnGioca().addActionListener(this);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		window.getLblGif().setVisible(false);
+		window.getLblPng().setVisible(false);
+		window.getLblPng2().setVisible(false);
+		window.getBtnGioca().setEnabled(false);
+		slot.gioca();
+		new Thread(() -> {
+			for (int i = 0; i < 11; i++) {
+				int cc = i;
+				String txt = "GIOCANDO";
+				SwingUtilities.invokeLater(() -> {
+					if (cc == 0 || cc == 4 || cc == 8) {
+						window.getLblVincita().setText(txt);
+					} else if (cc == 1 || cc == 5 || cc == 9) {
+						window.getLblVincita().setText(txt + ".");
+					} else if (cc == 2 || cc == 6 || cc == 10) {
+						window.getLblVincita().setText(txt + "..");
+					} else if (cc == 3 || cc == 7 || cc == 11) {
+						window.getLblVincita().setText(txt + "...");
+					}
+				});
+				for (int j = 0; j < 10; j++) {
+					int c = j;
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException ex) {
+						ex.printStackTrace();
+					}
+					SwingUtilities.invokeLater(() -> {
+						window.getBox1().setText(Integer.toString(c));
+						window.getBox2().setText(Integer.toString(c));
+						window.getBox3().setText(Integer.toString(c));
+					});
+				}
+			}
+			slot.generaNumeri();
+			int[] n = slot.getNumeri();
+			SwingUtilities.invokeLater(() -> {
+				window.getBox1().setText(Integer.toString(n[0]));
+				window.getBox2().setText(Integer.toString(n[1]));
+				window.getBox3().setText(Integer.toString(n[2]));
+			});
+			int nu = slot.getNumeriUguali();
+			if (nu == 0) {
+				window.getLblVincita().setText("Hai perso");
+			} else if (nu == 2) {
+				window.getLblPng().setVisible(true);
+				window.getLblVincita().setText("<html><center>HAI<br> VINTO 3 MONETE!</center></html>");
+			} else {
+				window.getLblPng().setVisible(true);
+				window.getLblPng2().setVisible(true);
+				window.getLblGif().setVisible(true);
+				window.getLblVincita().setText("<html><center>JACKPOT<br>HAI VINTO " + slot.getJackpot()
+						+ " MONETE!</center></html>");
+			}
+			window.getLblMonete().setText("<html><center>MONETE<br>" + Integer.toString(slot.getMonete())
+					+ "</center></html>");
+			window.getBtnGioca().setEnabled(true);
+		}).start();
+		
+	}
+	
 }
--- Slot-machine/src/view/GameWindow.java
+++ Slot-machine/src/view/GameWindow.java
@@ -10,7 +10,6 @@ import java.awt.GridLayout;
 import java.awt.Image;
 
 import javax.swing.JTextField;
-import javax.swing.UIManager;
 import javax.swing.Box;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -18,134 +17,174 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.Component;
 
 public class GameWindow extends JFrame {
-
-    private static final long serialVersionUID = 1L;
-    private JPanel contentPane;
-    private JTextField box1;
-    private JTextField box3;
-    private JTextField box2;
-    private JLabel lblVincita;
-    private JLabel lblMonete;
-    private JButton btnGioca;
-    private JLabel lblPng;
-    
-
-    
-    public JTextField getBox1() {
-        return box1;
-    }
-
-
-
-    public JTextField getBox3() {
-        return box3;
-    }
-
-
-
-    public JTextField getBox2() {
-        return box2;
-    }
-
-
-
-    public JLabel getLblVincita() {
-        return lblVincita;
-    }
-
-
-
-    public JLabel getLblMonete() {
-        return lblMonete;
-    }
-
-
-
-    public JButton getBtnGioca() {
-        return btnGioca;
-    }
-
-
-
-    public GameWindow() {
-    	addComponentListener(new ComponentAdapter() {
-    		@Override
-    		public void componentResized(ComponentEvent e) {
-    		lblMonete.setFont(new Font("Tahoma", Font.PLAIN, Math.max(8, contentPane.getWidth() / 40)));
-    		lblVincita.setFont(new Font("Tahoma", Font.PLAIN, Math.max(8, contentPane.getWidth() / 40)));
-    		box1.setFont(new Font("Tahoma", Font.PLAIN, Math.max(8, contentPane.getWidth() / 15)));
-    		box2.setFont(new Font("Tahoma", Font.PLAIN, Math.max(8, contentPane.getWidth() / 15)));
-    		box3.setFont(new Font("Tahoma", Font.PLAIN, Math.max(8, contentPane.getWidth() / 15)));
-    		btnGioca.setFont(new Font("Tahoma", Font.PLAIN, Math.max(8, contentPane.getWidth() / 20)));
-    		}
-    	});
-	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	setBounds(100, 100, 450, 300);
-	contentPane = new JPanel();
-	contentPane.setBackground(new Color(0, 0, 0));
-	contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-	setContentPane(contentPane);
-	contentPane.setLayout(new GridLayout(3, 4, 5, 0));
-	ImageIcon image = new ImageIcon(getClass().getResource("OK.png"));
-	ImageIcon scaledImage = new ImageIcon(image.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH));
-	lblPng = new JLabel(scaledImage);
 	
-	contentPane.add(lblPng);
-	contentPane.add(Box.createRigidArea(new Dimension(0, 0)));
-	contentPane.add(Box.createRigidArea(new Dimension(0, 0)));
-	contentPane.add(Box.createRigidArea(new Dimension(0, 0)));
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField box1;
+	private JTextField box3;
+	private JTextField box2;
+	private JLabel lblVincita;
+	private JLabel lblMonete;
+	private JButton btnGioca;
+	private JLabel lblPng;
+	private JLabel lblPng2;
+	private JLabel lblGif;
 	
-	box1 = new JTextField();
-	box1.setEnabled(false);
-	box1.setFont(new Font("Tahoma", Font.PLAIN, 30));
-	box1.setHorizontalAlignment(JLabel.CENTER);
-	box1.setBackground(Color.WHITE);
-	box1.setDisabledTextColor(Color.BLACK);
-	contentPane.add(box1);
-	box1.setColumns(10);
+	public JTextField getBox1() {
+		return box1;
+	}
 	
-	box2 = new JTextField();
-	box2.setEnabled(false);
-	box2.setFont(new Font("Tahoma", Font.PLAIN, 30));
-	box2.setHorizontalAlignment(JLabel.CENTER);
-	box2.setBackground(Color.WHITE);
-	box2.setDisabledTextColor(Color.BLACK);
-	contentPane.add(box2);
-	box2.setColumns(10);
+	public JTextField getBox3() {
+		return box3;
+	}
 	
-	box3 = new JTextField();
-	box3.setEnabled(false);
-	box3.setFont(new Font("Tahoma", Font.PLAIN, 30));
-	box3.setHorizontalAlignment(JLabel.CENTER);
-	box3.setBackground(Color.WHITE);
-	box3.setDisabledTextColor(Color.BLACK);
-	contentPane.add(box3);
-	box3.setColumns(10);
+	public JTextField getBox2() {
+		return box2;
+	}
 	
-	btnGioca = new JButton("GO!");
-	btnGioca.setBackground(new Color(255, 255, 255));
-	btnGioca.setFont(new Font("Tahoma", Font.PLAIN, 30));
-	btnGioca.setFocusPainted(false);
-	contentPane.add(btnGioca);
-	contentPane.add(Box.createRigidArea(new Dimension(0, 0)));
+	public JLabel getLblVincita() {
+		return lblVincita;
+	}
 	
-	lblVincita = new JLabel("");
-	lblVincita.setFont(new Font("Tahoma", Font.PLAIN, 11));
-	lblVincita.setHorizontalAlignment(JLabel.CENTER);
-	lblVincita.setForeground(Color.WHITE);
-	contentPane.add(lblVincita);
+	public JLabel getLblMonete() {
+		return lblMonete;
+	}
 	
+	public JButton getBtnGioca() {
+		return btnGioca;
+	}
 	
-	contentPane.add(Box.createRigidArea(new Dimension(0, 0)));
+	public JLabel getLblPng() {
+		return lblPng;
+	}
 	
-	lblMonete = new JLabel("Monete: ");
-	lblMonete.setFont(new Font("Tahoma", Font.PLAIN, 11));
-	lblMonete.setHorizontalAlignment(JLabel.CENTER);
-	lblMonete.setForeground(Color.WHITE);
-	contentPane.add(lblMonete);
-	setBackground(Color.BLACK);
-    }
-
+	public JLabel getLblPng2() {
+		return lblPng2;
+	}
+	
+	public JLabel getLblGif() {
+		return lblGif;
+	}
+	
+	public GameWindow() {
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(0, 0, 0));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(3, 4, 5, 0));
+		
+		lblPng = new JLabel();
+		ImageIcon image = new ImageIcon(GameWindow.class.getResource("/view/OK1.png"));
+		Image scaledImage = image.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+		lblPng.setIcon(new ImageIcon(scaledImage));
+		lblPng.setVisible(false);
+		
+		Component rigidArea = Box.createRigidArea(new Dimension(0, 0));
+		contentPane.add(rigidArea);
+		contentPane.add(lblPng);
+		contentPane.add(Box.createRigidArea(new Dimension(0, 0)));
+		contentPane.add(Box.createRigidArea(new Dimension(0, 0)));
+		
+		box1 = new JTextField();
+		box1.setEnabled(false);
+		box1.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		box1.setHorizontalAlignment(JLabel.CENTER);
+		box1.setBackground(Color.WHITE);
+		box1.setDisabledTextColor(Color.BLACK);
+		contentPane.add(box1);
+		box1.setColumns(10);
+		
+		box2 = new JTextField();
+		box2.setEnabled(false);
+		box2.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		box2.setHorizontalAlignment(JLabel.CENTER);
+		box2.setBackground(Color.WHITE);
+		box2.setDisabledTextColor(Color.BLACK);
+		contentPane.add(box2);
+		box2.setColumns(10);
+		
+		box3 = new JTextField();
+		box3.setEnabled(false);
+		box3.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		box3.setHorizontalAlignment(JLabel.CENTER);
+		box3.setBackground(Color.WHITE);
+		box3.setDisabledTextColor(Color.BLACK);
+		contentPane.add(box3);
+		box3.setColumns(10);
+		
+		btnGioca = new JButton("GO!");
+		btnGioca.setBackground(new Color(255, 255, 255));
+		btnGioca.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnGioca.setFocusPainted(false);
+		contentPane.add(btnGioca);
+		
+		lblGif = new JLabel();
+		ImageIcon gif = new ImageIcon(GameWindow.class.getResource("/view/OK3.gif"));
+		Image scaledGif = gif.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+		lblGif.setIcon(new ImageIcon(scaledGif));
+		lblGif.setVisible(false);
+		contentPane.add(lblGif);
+		
+		lblVincita = new JLabel("");
+		lblVincita.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		lblVincita.setHorizontalAlignment(JLabel.CENTER);
+		lblVincita.setForeground(Color.WHITE);
+		contentPane.add(lblVincita);
+		
+		lblPng2 = new JLabel();
+		ImageIcon image2 = new ImageIcon(GameWindow.class.getResource("/view/OK2.png"));
+		Image scaledImage2 = image2.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+		lblPng2.setIcon(new ImageIcon(scaledImage2));
+		lblPng2.setVisible(false);
+		contentPane.add(lblPng2);
+		
+		lblMonete = new JLabel("Monete: ");
+		lblMonete.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		lblMonete.setHorizontalAlignment(JLabel.CENTER);
+		lblMonete.setForeground(Color.WHITE);
+		contentPane.add(lblMonete);
+		setBackground(Color.BLACK);
+		addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentResized(ComponentEvent e) {
+				fontSet(lblPng,image);
+				fontSet(lblPng2,image2);
+				fontSet(lblGif,gif);
+				fontSet(box1);
+				fontSet(box2);
+				fontSet(box3);
+				fontSet(lblMonete);
+				fontSet(lblVincita);
+				fontSet(btnGioca);
+			}
+		});
+	}
+	private void fontSet(JLabel lbl, ImageIcon i) {
+		Image scaledImage = i.getImage().getScaledInstance((int) (contentPane.getWidth() / 4.5), contentPane.getHeight()
+				/ 3, Image.SCALE_DEFAULT);
+		lbl.setIcon(new ImageIcon(scaledImage));
+	}
+	private void fontSet(Object i) {
+		int w = contentPane.getWidth();
+		if(i.getClass()==JLabel.class) {
+			 JLabel j = (JLabel) i;
+			 j.setFont(new Font("Tahoma", Font.PLAIN, Math.max(8, w / 40)));
+		}
+		else if(i.getClass()==JButton.class) {
+			JButton j = (JButton) i;
+			j.setFont(new Font("Tahoma", Font.PLAIN, Math.max(8, w / 15)));
+		}
+		else if(i.getClass()==JTextField.class) {
+			JTextField j = (JTextField) i;
+			j.setFont(new Font("Tahoma", Font.PLAIN, Math.max(8, w / 20)));
+			
+		}
+	}
+	
 }
